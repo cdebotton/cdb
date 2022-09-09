3873859e92253aac73720b8ca0c9b6562269c321
Christian de Botton <416880+cdebotton@users.noreply.github.com>
2022-09-09 00:51:34 -0700

    Refresh Token

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,24 +1,28 @@
-import { parse } from 'cookie';
-
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-	/** @type {Record<string, string> | null} */
-	let cookie = null;
-	let headerValue = event.request.headers.get('cookie');
-	if (headerValue) {
-		cookie = parse(headerValue);
-	}
+	let refreshToken = event.cookies.get('refresh_token');
+	let accessToken = event.cookies.get('access_token');
 
-	if (cookie?.refresh_token && !cookie?.access_token) {
+	if (refreshToken && !accessToken) {
 		try {
+			let result = await fetch('http://localhost:3000/accounts/revalidate', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ refresh_token: refreshToken })
+			}).then((res) => res.json());
+
+			console.log(result, '!!!');
 			// Request endpoint to set new access token and update refresh token.
 		} catch (err) {
+			console.log(err);
 			// Yikes. Delete both tokens.
 		}
 	}
 
-	event.locals.accessToken = cookie?.access_token;
-	event.locals.refreshToken = cookie?.refresh_token;
+	event.locals.accessToken = accessToken;
+	event.locals.refreshToken = refreshToken;
 
 	let response = await resolve(event);
 
